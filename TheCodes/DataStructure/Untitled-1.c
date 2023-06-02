3d5c7f022058a150f81b8e157f55103c2508d17b
GreenSheep-Studio <wsdx54@163.com>
2023-06-02 22:31:28 +0800

    BitsTreeSearch(2 bugs)

---
+++ TheCodes/DataStructure/Untitled-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+    printf ("Hello");
+    system("PAUSE");
+    return 0;
+}
--- TheCodes/SomeJunk/Untitled-1.cpp
+++ TheCodes/SomeJunk/Untitled-1.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int Print()
+{int i=1;
+int x=(int)sqrt(n);
+while (++i<=x)
+{
+    if (n)
+    {
+        /* code */
+    }
+    
+}
+}
+void sum(int a[])
+{a[0] = ++a[1];}
 int main()
 {
-    return 0;
     
+    int a[10] = {10,9,8,7,6,5,4,3};
+    sum(&a[2]);
+    cout << a[2] <<endl;
+    system("PAUSE");
+    return 0;
 }
